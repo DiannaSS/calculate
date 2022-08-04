4184a426a04a9d9dde4bf2e1e647feb030ecd16f
Ситдикова Диана Салаватовна <103957474+DiannaSS@users.noreply.github.com>
2022-08-04 18:54:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-public class Main  {
+public class Main {
     public static void main(String[] args) {
         int amount = 400;
-        int mili= amount/20;
+        int mili = amount / 20;
         System.out.println(mili)
-    }}
+    }
+}
